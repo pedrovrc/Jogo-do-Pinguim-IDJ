49e2488d91494f64a5b338032ac3fcb68cfdecdd
Pedro Victor <43827142+pedrovrc@users.noreply.github.com>
2023-04-25 17:37:18 -0300

    Update 0.2.05
    Inicio da implementação de rotação em Vec2

---
+++ Vec2.h
@@ -0,0 +1,32 @@
+#ifndef VEC2_H
+#define VEC2_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Vec2 {
+	Vec2& Vec2Add(Vec2 vector);
+	Vec2& Vec2MultiplyScalar(float scalar);
+public:
+	float x;
+	float y;
+	Vec2();
+	Vec2(float x, float y);
+	float GetMagnitude();
+	Vec2& GetNormalizedVector();
+	float GetDistance(Vec2 point);
+	Vec2& Rotate(float angle);
+
+	Vec2& operator+(Vec2& b);
+	Vec2& operator-(Vec2& b);
+	Vec2& operator*(float scalar);
+	Vec2& operator/(float scalar);
+
+	// implementar:
+	// - inclinacao de um vetor
+	// - inclinacao da reta dada por dois pontos
+	// - rotacao em um determinado angulo
+
+};
+
+#endif
