e34f46dd01cc678a1291468443a08dc6c701128b
pedrovrc <pedrovrc32@gmail.com>
2023-05-28 17:10:02 -0300

    Update 0.5.03
    Retirada de prints e código de teste. Velocidade do Alien aumentada.

--- Jogo do Pinguim/Alien.cpp
+++ Jogo do Pinguim/Alien.cpp
@@ -4,7 +4,7 @@
 #include "Camera.h"
 
 #define MAX_HP 30
-#define MAX_SPEED 3
+#define MAX_SPEED 30
 
 Alien::Action::Action(ActionType type, float x, float y) {
 	pos.x = x;
@@ -34,26 +34,6 @@ void Alien::Start() {
 void Alien::Update(float dt) {
 	InputManager* input = &InputManager::GetInstance();
 
-	//teste
-//	if(input->MousePress(LEFT_MOUSE_BUTTON) || input->MousePress(RIGHT_MOUSE_BUTTON)) {
-//		int mouseX = input->GetMouseX() - Camera::pos.x;
-//		int mouseY = input->GetMouseY() - Camera::pos.y;
-//		if (input->MousePress(LEFT_MOUSE_BUTTON)) {
-//			if (flag) {
-//				a.Set(mouseX, mouseY);
-//			} else {
-//				b.Set(mouseX, mouseY);
-//			}
-//			cout << "MouseX = " << mouseX << ", MouseY = " << mouseY << endl;
-//			cout << "Vec2 a: (" << a.x << "," << a.y << ")" << endl;
-//			cout << "Vec2 b: (" << b.x << "," << b.y << ")" << endl;
-//			double distance = a.GetDistance(b);
-//			cout << "Distancia = " << distance << endl;
-//			cout << "--------------------------------------" << endl << endl;
-//			flag = !flag;
-//		}
-//	}
-
 	// coloca acoes na fila
 	if(input->MousePress(LEFT_MOUSE_BUTTON) || input->MousePress(RIGHT_MOUSE_BUTTON)) {
 		int mouseX = input->GetMouseX() - Camera::pos.x;
@@ -73,22 +53,14 @@ void Alien::Update(float dt) {
 	if (taskQueue.empty() == false) {
 		act = &taskQueue.front();
 		if (act->type == Action::ActionType::MOVE) {
-			cout << "act->pos = (" << act->pos.x << "," << act->pos.y << ")" << endl;
-			cout << "box = (" << associated.box.GetCenter().x << "," << associated.box.GetCenter().y << ")" << endl;
-			cout << "Distance = " << act->pos.GetDistance(associated.box.GetCenter()) << endl;
-			cout << "-------------------------" << endl;
 			if (act->pos.GetDistance(associated.box.GetCenter()) >= MAX_SPEED) {
 				Vec2 direction = act->pos - associated.box.GetCenter();
 				speed = direction.GetNormalizedVector() * MAX_SPEED;
 				associated.box.MoveThis(speed);
 			} else {
-				cout << "DONE! ------------------------" << endl;
-				cout << "act->pos: ("<< act->pos.x << "," << act->pos.y << ")" << endl;
-				cout << "box before: ("<< associated.box.GetCenter().x << "," << associated.box.GetCenter().y << ")" << endl;
 				Vec2 offset;
 				offset.Set(associated.box.w/2, associated.box.h/2);
 				associated.box.SetPosition(act->pos - offset);
-				cout << "box after: ("<< associated.box.GetCenter().x << "," << associated.box.GetCenter().y << ")" << endl;
 				taskQueue.pop();
 				speed.Set(0,0);
 			}
